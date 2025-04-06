c2ecba7fe8762ca62fcea7d82d4f27a25f15c1f1
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-04-07 00:00:49 +0300

    Update
    • HelloWorld.py renamed.
    • Added comments to all .py files.
    • Added loop.py. Loops topic was shown in outline.

--- 0 - Python Mini Education Set/0 - Hello World/helloworld.py
+++ 0 - Python Mini Education Set/0 - Hello World/helloworld.py
@@ -1,1 +1,2 @@
+## With this, we print Hello World on the screen.
 print("Hello World")
---
+++ 0 - Python Mini Education Set/2 - Loop/Loop.py
@@ -0,0 +1,3 @@
+## With this loop, we print the numbers from 1 to 5. In this case, 5 is not included.
+for i in range(5):
+    print(i)
