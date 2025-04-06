b76f30a14210aff2bb76584a5f8f067a495bf03d
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-04-07 00:41:03 +0300

    Dictionaries.py
    • Codes related to dictionaries logic have been added.

---
+++ 0 - Python Mini Education Set/5 - Dictionaries/Dictionaries.py
@@ -0,0 +1,6 @@
+## 
+sozluk = {"anahtar_1": "değer_1", "anahtar_2": 3, "abc": "Yiğithan"}
+
+print(sozluk["anahtar_1"])  # değer_1
+print(sozluk.keys())  # ['abc', 'anahtar_2', 'anahtar_1']
+print(sozluk.values())  # ['Yiğithan', 3, 'değer_1']
