ebcc91ed56f7d0c1ee7a02a1c97f8a875a597615
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-04-07 00:16:18 +0300

    Update
    • Codes for loops were added.
    
    • Condition checks were made and outputs were given to the screen.

---
+++ 0 - Python Mini Education Set/2 - Koşullar/Condition.py
@@ -0,0 +1,10 @@
+## The condition was checked according to the value of X and then the output was given to the screen.
+x = 5
+if x == 5:
+    print("x is equal to 5.")
+elif x == 4:
+    print("x is not equal to 5. x =", x)
+elif x != 5:
+    print("x is not equal to 5.")
+else:
+    print("x what?")
